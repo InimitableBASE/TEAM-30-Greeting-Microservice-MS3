013b8b9530416aa950755fd38c4c07b8c52242ec
Justin Holley <jholley6@gmail.com>
2025-11-02 16:23:21 -0700

    Removed unnecessary comments from _testGreeting.py

--- _testGreeting.py
+++ _testGreeting.py
@@ -12,6 +12,7 @@ def main():
     # Create folder if it doesn't exist
     os.makedirs(folder_path, exist_ok=True)
 
+    ### Loop used for Request/Response to greeting.py
     while True:
         # Ask user for name
         name = input("\nEnter your name (or type 'STOP' to quit): ").strip()
@@ -33,40 +34,6 @@ def main():
             content = file.read()
 
         print(f"greeting.txt now says: \n{content}\n")
-
-
-    # ### TEST CASES
-    # # Test Cases for Time of Day
-    # date = datetime.now()
-    # yr = date.year
-    # month = date.month
-    # day = date.day
-
-
-
-    # print(f"\nTESTING 4:59 AM WITH NAME: BUGS BUNNY:")
-    # hr = 4
-    # min = 59
-    # fake_now = datetime(yr, month, day, hr, min, 0)
-    # with patch("greeting.datetime") as mock_datetime:
-    #     # Make datetime.now() return our fake time
-    #     mock_datetime.now.return_value = fake_now
-    #     # Let datetime.strftime still behave normally
-    #     mock_datetime.strftime = datetime.strftime
-    #     print(build_greeting("First Last"))
-
-    # # Test Cases for Holidays
-    # print(f"\nTESTING CHRISTMAS MORNING AT 8:30 AM WITH NAME: BUGS BUNNY:")
-    # fake_now = datetime(2025, 12, 25, 8, 30, 0)
-    # with patch("greeting.datetime") as mock_datetime:
-    #     # Make datetime.now() return our fake time
-    #     mock_datetime.now.return_value = fake_now
-    #     # Let datetime.strftime still behave normally
-    #     mock_datetime.strftime = datetime.strftime
-    #     print(build_greeting("First Last"))
-
-
-
 
 if __name__ == "__main__":
     main()
